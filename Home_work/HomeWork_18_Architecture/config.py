411fe824fc1be903000c9cc30a78b5f957c3caad
Vasilii <103426030+vase09@users.noreply.github.com>
2022-07-29 19:57:23 +0300

    Delete Home_work/HomeWork_18_Architecture directory

--- Home_work/HomeWork_18_Architecture/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RESTX_JSON = {'ensure_ascii': False, 'indent': 4}
-
-
-"""Файл с настройками."""
