2cc4a73e90b36ca98be54de9ba25404d5dc25ef0
Пользователь <Vase09@yandex.ru>
2022-08-24 16:46:00 +0300

    -

--- course_work4-dev/course_work4-dev/config.py
+++
@@ -1,8 +0,0 @@
-class Config(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///movies.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RESTX_JSON = {'ensure_ascii': False, 'indent': 2}
-    SECRET = '3CVDx#4cvs'
-    ALGORITHM = ''
-
